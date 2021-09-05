b25c2c35996574fedde364767965573f743ae8dc
LOAN <86809441+WorldLoanFinance@users.noreply.github.com>
2021-09-05 18:21:20 -0400

    Create IOracle.sol

---
+++ IOracle.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.6.0;
+
+import "./IPriceConsumer.sol";
+
+interface IOracle is IPriceConsumer {
+    function queryAllPrices() external view returns (
+        address[] memory assets,
+        uint[] memory prices,
+        uint[] memory lastUpdatedTimes
+    );
+}
